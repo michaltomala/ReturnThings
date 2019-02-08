1f85591594bad992a940abb2b1006668de681d02
Michał Tomala <michaltomala63@gmail.com>
2019-02-08 23:48:26 +0100

    configuration

---
+++ src/main/java/pl/coderslab/app/FormatterConfig.java
@@ -0,0 +1,31 @@
+package pl.coderslab.app;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+@ComponentScan("pl.coderslab")
+@EnableWebMvc
+@EnableTransactionManagement
+public class FormatterConfig implements WebMvcConfigurer{
+
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+
+//        registry.addConverter(getUserConverter());
+
+    }
+
+//    @Bean
+//    public UserConverter getUserConverter(){ return new UserConverter(); }
+
+
+
+}
