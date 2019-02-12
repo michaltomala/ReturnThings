f5109924b006ed5a47872b9526881564da8ca622
Michał Tomala <michaltomala63@gmail.com>
2019-02-12 14:58:26 +0100

    created: InitAppService - create admin if app hasn't admins yet

--- src/main/java/pl/coderslab/controllers/RegisterController.java
+++ src/main/java/pl/coderslab/controllers/RegisterController.java
@@ -13,4 +13,5 @@ public class RegisterController {
         return "register";
     }
 
+
 }
---
+++ src/main/java/pl/coderslab/services/InitApplicationService.java
@@ -0,0 +1,31 @@
+package pl.coderslab.services;
+
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.coderslab.entity.User;
+import pl.coderslab.repository.UserRepository;
+
+@Service
+public class InitApplicationService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    /**
+     * In my app there is always minimum one admin - created during server started
+     */
+    public void initAdmin(){
+        System.out.println("INIT APP");
+        User userToCheck = userRepository.findOne((long) 1);
+        if( userToCheck == null) {
+            User user = new User();
+            user.setEmail("admin@admin.pl");
+            user.setPassword("123456");
+            user.setAdmin(true);
+            user.setPassword(BCrypt.hashpw(user.getPassword(), BCrypt.gensalt()));
+            userRepository.save(user);
+        }
+    }
+
+}
