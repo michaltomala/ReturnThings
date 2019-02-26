378d660fc589602259e0e77be1647200e0d1b202
Michał Tomala <michaltomala63@gmail.com>
2019-02-26 17:52:19 +0100

    filter for not logged users

--- src/main/java/pl/coderslab/filter/AuthFilter.java
+++ src/main/java/pl/coderslab/filter/AuthFilter.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 
-@WebFilter(filterName = "AuthFilter", urlPatterns = {"/login","/register"})
+@WebFilter(filterName = "AuthFilter", urlPatterns = {"/login","/register","/home"})
 public class AuthFilter implements Filter {
 
     public void destroy() {
@@ -29,6 +29,14 @@ public class AuthFilter implements Filter {
                 return;
             }
         }
+
+        if( (url.equals("/home"))) {
+            if(session.getAttribute("user") == null){
+                response.sendRedirect(request.getContextPath()+"/login");
+                return;
+            }
+        }
+
         chain.doFilter(req, resp);
     }
 
