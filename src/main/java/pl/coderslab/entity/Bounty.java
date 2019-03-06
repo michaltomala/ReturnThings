2d5fd77d660a6feab88d2e5f091c281f8568ae64
Michał Tomala <michaltomala63@gmail.com>
2019-03-06 09:38:52 +0100

    components of entity Bounty

--- src/main/java/pl/coderslab/entity/Bounty.java
+++ src/main/java/pl/coderslab/entity/Bounty.java
@@ -1,10 +1,15 @@
 package pl.coderslab.entity;
 
 
+import org.hibernate.validator.constraints.NotBlank;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Entity
@@ -14,15 +19,37 @@ public class Bounty {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+
+    @NotBlank(message = "Musisz zaznaczyć co chcesz oddać!")
+    private String name;
+
+    @NotNull
+    private Long quantityOfBags;
+
     public Bounty() {
     }
 
+
+    public static List<String> listOfBountyNames (){
+        return new ArrayList<String>() {
+            {
+                add("ubrania, które nadają się do ponownego użycia");
+                add("ubrania do wyrzucenia");
+                add("zabawki");
+                add("książki");
+                add("inne");
+            }
+        };
+    }
+
+
+
     @Override
     public String toString() {
-        return "Bounty{" +
-                "id=" + id +
-                '}';
+        return this.name;
     }
+
+
 
     public Long getId() {
         return id;
@@ -30,5 +57,21 @@ public class Bounty {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getQuantityOfBags() {
+        return quantityOfBags;
+    }
+
+    public void setQuantityOfBags(Long quantityOfBags) {
+        this.quantityOfBags = quantityOfBags;
     }
 }
--- src/main/java/pl/coderslab/entity/Institution.java
+++ src/main/java/pl/coderslab/entity/Institution.java
@@ -5,7 +5,6 @@ import org.hibernate.validator.constraints.NotBlank;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
