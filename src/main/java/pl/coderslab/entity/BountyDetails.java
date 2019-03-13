0055268cfd26dd5e9d70fe7af0aa486fb2139cde
Michał Tomala <michaltomala63@gmail.com>
2019-03-13 14:37:54 +0100

    add components of BountyDetails

--- src/main/java/pl/coderslab/entity/BountyDetails.java
+++ src/main/java/pl/coderslab/entity/BountyDetails.java
@@ -4,15 +4,55 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Date;
 
 
 @Entity
-public class BountyDetails {
+public class BountyDetails extends Bounty{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private boolean received;
+    private Date dateReceived;
+    private Date dateRegistration;
 
 
+    public BountyDetails() {
+    }
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public boolean isReceived() {
+        return received;
+    }
+
+    public void setReceived(boolean received) {
+        this.received = received;
+    }
+
+    public Date getDateReceived() {
+        return dateReceived;
+    }
+
+    public void setDateReceived(Date dateReceived) {
+        this.dateReceived = dateReceived;
+    }
+
+    public Date getDateRegistration() {
+        return dateRegistration;
+    }
+
+    public void setDateRegistration(Date dateRegistration) {
+        this.dateRegistration = dateRegistration;
+    }
 }
